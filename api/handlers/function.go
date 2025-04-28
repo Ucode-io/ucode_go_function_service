f5eb0242625e76a5af331604585325f19a053123
OtabekMuzropov <otabek.muzropov2001@gmail.com>
2025-04-28 09:09:59 +0500

    update max scale function

--- api/handlers/function.go
+++ api/handlers/function.go
@@ -531,18 +531,34 @@ func (h *Handler) UpdateFunction(c *gin.Context) {
 			h.handleResponse(c, status.GRPCError, err.Error())
 			return
 		}
-	case pb.ResourceType_POSTGRESQL:
-		updateFunction := &nb.Function{}
 
-		if err = helper.MarshalToStruct(updateFunction, &updateFunction); err != nil {
+		if updateFunction.MaxScale != updateFunction.FormerMaxScale {
+			err = h.AlterScale(updateFunction.Path, updateFunction.MaxScale)
+			if err != nil {
+				h.handleResponse(c, status.InternalServerError, err.Error())
+				return
+			}
+		}
+	case pb.ResourceType_POSTGRESQL:
+		function := &nb.Function{}
+
+		if err = helper.MarshalToStruct(updateFunction, &function); err != nil {
 			h.handleResponse(c, status.InternalServerError, err.Error())
 			return
 		}
 
-		resp, err = h.services.GoObjectBuilderService().Function().Update(ctx, updateFunction)
+		resp, err = h.services.GoObjectBuilderService().Function().Update(ctx, function)
 		if err != nil {
 			h.handleResponse(c, status.GRPCError, err.Error())
 			return
+		}
+
+		if updateFunction.MaxScale != updateFunction.FormerMaxScale {
+			err = h.AlterScale(updateFunction.Path, updateFunction.MaxScale)
+			if err != nil {
+				h.handleResponse(c, status.InternalServerError, err.Error())
+				return
+			}
 		}
 	}
 
@@ -1349,18 +1365,10 @@ func (h *Handler) InvokeFuncByApiPath(c *gin.Context) {
 	c.JSON(statusCode, resp)
 }
 
-func (h *Handler) AlterScale(c *gin.Context) {
-	m := make(map[string]any)
-
-	err := c.ShouldBindJSON(&m)
-	if err != nil {
-		h.handleResponse(c, status.InvalidArgument, err.Error())
-		return
-	}
-
+func (h *Handler) AlterScale(name string, maxScale int32) error {
 	token, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/token")
 	if err != nil {
-		fmt.Println("Error reading token file:", err)
+		return err
 	}
 
 	fmt.Println("GOT TOKEN:", string(token))
@@ -1372,7 +1380,7 @@ func (h *Handler) AlterScale(c *gin.Context) {
 	fmt.Println("Kube host:", kubeHost)
 	fmt.Println("Kube port:", kubePort)
 
-	url := fmt.Sprintf("https://%s:%s/apis/serving.knative.dev/v1/namespaces/knative-fn/services/%s", kubeHost, kubePort, m["name"])
+	url := fmt.Sprintf("https://%s:%s/apis/serving.knative.dev/v1/namespaces/knative-fn/services/%s", kubeHost, kubePort, name)
 
 	payload := map[string]any{
 		"spec": map[string]any{
@@ -1380,7 +1388,7 @@ func (h *Handler) AlterScale(c *gin.Context) {
 				"metadata": map[string]any{
 					"annotations": map[string]any{
 						"autoscaling.knative.dev/minScale": "0",
-						"autoscaling.knative.dev/maxScale": cast.ToString(m["max_scale"]),
+						"autoscaling.knative.dev/maxScale": fmt.Sprintf("%d", maxScale),
 					},
 				},
 			},
@@ -1389,16 +1397,13 @@ func (h *Handler) AlterScale(c *gin.Context) {
 
 	payloadByte, err := json.Marshal(payload)
 	if err != nil {
-		h.handleResponse(c, status.InvalidArgument, err.Error())
-		return
+		return err
 	}
 
 	// Prepare the HTTP PATCH request
 	req, err := http.NewRequest(http.MethodPatch, url, bytes.NewBuffer(payloadByte))
 	if err != nil {
-		fmt.Println("Error creating request:", err)
-		h.handleResponse(c, status.InvalidArgument, err.Error())
-		return
+		return err
 	}
 	req.Header.Set("Authorization", "Bearer "+string(token))
 	req.Header.Set("Content-Type", "application/merge-patch+json")
@@ -1412,22 +1417,20 @@ func (h *Handler) AlterScale(c *gin.Context) {
 	// Send the request
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error sending request:", err)
-		h.handleResponse(c, status.InternalServerError, err.Error())
-		return
+		return err
 	}
 	defer resp.Body.Close()
 
 	// Read and print the response
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		h.handleResponse(c, status.InternalServerError, err.Error())
-		return
+		return err
 	}
 
 	fmt.Printf("Status: %s\n", resp.Status)
 	fmt.Println("Response: ", string(body))
+
+	return nil
 }
 
 func (h *Handler) ExecKnative(path string, req models.NewInvokeFunctionRequest) (models.InvokeFunctionResponse, error) {
